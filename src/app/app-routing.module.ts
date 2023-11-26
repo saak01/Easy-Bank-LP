d33ade628f6fb79f6ab69c2a7ec99ad59a96def9
saak01 <joaovictorcosta.dev@gmail.com>
2023-11-26 15:40:13 -0300

    header feito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeRootComponent } from './pages/home/components/home-root/home-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"welcome", component: HomeRootComponent},
+  {path:"", redirectTo:"welcome", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeRootComponent } from './pages/home/components/home-root/home-root.component';
 import { HeaderComponent } from './pages/home/components/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './pages/home/components/header/header.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
