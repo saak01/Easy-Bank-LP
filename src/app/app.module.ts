a7c56c324e45ba62b9cd337ca21922ed8e1af261
saak01 <joaovictorcosta.dev@gmail.com>
2023-11-26 14:21:03 -0300

    adição angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeRootComponent } from './pages/home/components/home-root/home-root.component';
+import { HeaderComponent } from './pages/home/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeRootComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home/components/home-root/home-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-root',
+  templateUrl: './home-root.component.html',
+  styleUrls: ['./home-root.component.scss']
+})
+export class HomeRootComponent {
+
+}
